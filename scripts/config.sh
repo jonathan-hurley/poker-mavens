ca833f944fc8441b2988c0352b08b25c79c0af02
Jonathan Hurley <jonathanhurley@apache.org>
2022-04-03 19:24:44 -0400

    Trying to detect PM6 vs PM7 (for now)

--- scripts/config.sh
+++ scripts/config.sh
@@ -12,8 +12,17 @@ case "$(uname -s)" in
      ;;
 
    CYGWIN*|MINGW32*|MSYS*|MINGW*)
-    # bash on windows has problems with spaces in the directory names
-    POKER_MAVENS_HOME_DIR="/c/Users/Administrator/AppData/Roaming/pokerm~1"
+    # bash on windows has problems with spaces in the directory names so use one without spaces
+    # first check to see if PM7 is installed and use that, otherwise check for PM6
+    POKER_MAVENS6_HOME_DIR="/c/Users/Administrator/AppData/Roaming/pokerm~1"
+    POKER_MAVENS7_HOME_DIR="/c/Users/Public/Documents/Briggs~1/PokerM~1"   
+
+    if [ -d "$POKER_MAVENS7_HOME_DIR" ] 
+    then
+      POKER_MAVENS_HOME_DIR=$POKER_MAVENS7_HOME_DIR
+    else
+      POKER_MAVENS_HOME_DIR=$POKER_MAVENS6_HOME_DIR
+    fi
 
     # add required GNU programs to the path
     PATH="$PATH:/c/Program Files (x86)/GnuWin32/bin"
