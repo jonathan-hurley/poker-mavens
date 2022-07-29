aacc2315fcbf11d6490ae4a2c879b54d02ceedee
Jonathan Hurley <jonathanhurley@apache.org>
2022-07-29 13:12:08 -0400

    Adding ESPT as the main chips with a variety of DBs

---
+++ scripts/players.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Lists all players to generate data for
+PLAYERS=( "Andrew"
+)
+
+# Missing cash data
+declare -A PLAYER_CASH_OFFSET=(
+  ["Player-1"]="0"
+  ["Player-2"]="240.50"
+  ["default"]="0"
+)
+
+# Missing tournament data
+declare -A PLAYER_TOURNAMENT_WINNINGS_OFFSET=(
+  ["Player-1"]="0"
+  ["Player-2"]="240.50"
+  ["default"]="0"
+)
+
+# Missing number of tournament cashes data
+declare -A PLAYER_NUM_CASHES_OFFSET=(
+  ["Player-1"]="1"
+  ["Player-2"]="2"
+  ["default"]="0"
+)
